b4b35aa9dace13d90db4ed8a75707011aec6b3ec
Lukamam007 <lukamam007@gmail.com>
2025-05-27 13:54:26 -0300

    Update pilas.h

--- pilas.h
+++ pilas.h
@@ -1,12 +1,28 @@
+#ifndef PILA_H
+#define PILA_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "nivel.h" 
+
+typedef struct {
+    Coordenada posicion;
+    int idx_torre;
+} Estado;
+
+extern int MAX_PILA;
+
 typedef struct _Pila {
-    int *datos;
+    Estado *datos;
     int ultimo;
 } Pila;
 
 Pila* pila_crear();
-int pila_es_vacia(Pila*); 
-int pila_tope(Pila*); 
-void pila_apilar(Pila*,int); 
-void pila_desapilar(Pila*); 
-void pila_imprimir(Pila*); 
-void pila_destruir(Pila*); 
+int pila_es_vacia(Pila*);
+Estado pila_tope(Pila*);
+void pila_apilar(Pila*, Estado);
+void pila_desapilar(Pila*);
+void pila_imprimir(Pila*);
+void pila_destruir(Pila*);
+
+#endif
