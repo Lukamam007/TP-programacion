a5a24d22405741a480c1871f9ad3daa61a743276
BenicioPoli <beniciosanchezmandato@gmail.com>
2025-05-29 20:49:12 -0300

    Update estrategia.c

--- estrategia.c
+++ estrategia.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include "pilas.h"
 #include "turno.h"
 
 static int posiciones_validas(Coordenada *validas, TipoCasilla **casillas, int alto, int ancho) {
