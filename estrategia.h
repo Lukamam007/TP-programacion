058e0c6c3981ce98f8086b4692b17a6999ec9042
BenicioPoli <beniciosanchezmandato@gmail.com>
2025-05-28 22:42:51 -0300

    Cosas para version final custom

--- estrategia.h
+++ estrategia.h
@@ -7,12 +7,20 @@
 
 typedef void (*DisposicionTorres)(Nivel*, Mapa*);
 
+typedef struct _area_ataque_de_casillas{
+	      int x,y;
+        int ataque;
+}area_ataque_de_casillas;
+
 void disponer(Nivel* nivel, Mapa* mapa);
 
 void disponer_con_backtracking(Nivel* nivel, Mapa* mapa);
 
 void disponer_custom(Nivel* nivel, Mapa* mapa);
 
-int contarVecinosCamino(int posicion_torre_x, int posicion_torre_y , Mapa* mapa);
+int contarVecinosCamino(int posicion_torre_x, int posicion_torre_y , Mapa* mapa,int distancia_ataque);
+
+int comparar(const void *a, const void *b);
+
 
 #endif
